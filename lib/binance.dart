ff82bf117f87409e6441ea0ff358b296d735168d
Luke Pighetti <lukepighetti@gmail.com>
2019-02-15 12:12:03 -0500

    Library documentation

--- lib/binance.dart
+++ lib/binance.dart
@@ -1,3 +1,15 @@
+/// ## Binance
+///
+/// Binance has excellent documentation on their Websocket and REST APIs. We convert all endpoint
+/// JSON objects into Dart classes for easy consumption.
+///
+/// Websockets
+/// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md
+///
+/// REST
+/// https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
+///
+
 library binance;
 
 export 'data/rest_classes.dart';
