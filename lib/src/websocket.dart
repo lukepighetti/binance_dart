0e55b0d074300c565e5a5f739d2ef9ec627bb6bf
Luke Pighetti <lukepighetti@gmail.com>
2019-02-15 12:25:46 -0500

    basic websocket.dart documentation

--- lib/src/websocket.dart
+++ lib/src/websocket.dart
@@ -13,6 +13,8 @@ class BinanceWebsocket {
   List<Map> _toList(json) => List<Map>.from(convert.jsonDecode(json));
 
   /// Reports aggregated trade events from <symbol>@aggTrade
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#aggregate-trade-streams
   Stream<WsAggregatedTrade> aggTrade(String symbol) {
     final channel = _public('${symbol.toLowerCase()}@aggTrade');
 
@@ -22,6 +24,8 @@ class BinanceWebsocket {
   }
 
   /// Reports 24hr miniTicker events every second from <symbol>@miniTicker
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#individual-symbol-mini-ticker-stream
   Stream<MiniTicker> miniTicker(String symbol) {
     final channel = _public('${symbol.toLowerCase()}@miniTicker');
 
@@ -32,6 +36,8 @@ class BinanceWebsocket {
 
   /// Reports 24hr miniTicker events every second for every trading pair
   /// that changed in the last second
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#all-market-mini-tickers-stream
   Stream<List<MiniTicker>> allMiniTickers() {
     final channel = _public('!miniTicker@arr');
 
@@ -40,6 +46,8 @@ class BinanceWebsocket {
   }
 
   /// Reports 24hr ticker events every second from <symbol>@ticker
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#individual-symbol-ticker-streams
   Stream<Ticker> ticker(String symbol) {
     final channel = _public('${symbol.toLowerCase()}@ticker');
 
@@ -50,6 +58,8 @@ class BinanceWebsocket {
 
   /// Reports 24hr miniTicker events every second for every trading pair
   /// that changed in the last second
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#all-market-tickers-stream
   Stream<List<Ticker>> allTickers() {
     final channel = _public('!ticker@arr');
 
@@ -61,6 +71,8 @@ class BinanceWebsocket {
   /// Reports book depth
   ///
   /// Levels can be 5, 10, or 20
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#partial-book-depth-streams
   Stream<BookDepth> bookDepth(String symbol, [int levels = 5]) {
     assert(levels == 5 || levels == 10 || levels == 20);
 
@@ -74,6 +86,8 @@ class BinanceWebsocket {
   /// Difference book depth
   ///
   /// This can be used to update an existing book with incremental changes
+  ///
+  /// https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md#diff-depth-stream
   Stream<DiffBookDepth> diffBookDepth(String symbol) {
     final channel = _public('${symbol.toLowerCase()}@depth');
 
